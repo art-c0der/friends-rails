a19823716fabb859605a57d87c65bff5a4091fa1
Artem Vorobiov <artem.vorobjov@gmail.com>
2023-12-13 21:16:24 -0500

    add bootstrap

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get home_indeex_url
     assert_response :success
   end
